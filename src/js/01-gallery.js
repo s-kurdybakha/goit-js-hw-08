0793190fb6f3432a126c4c82968a855d61750784
Sergii Kurdybakha <s.kurdybakha@gmail.com>
2023-11-11 22:41:30 +0200

    Task 1 completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
+import Simplelightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map((image) => 
+`<li><a class="gallery__item" href=${image.original}>
+  <img
+      class="gallery__image"
+      src=${image.preview}
+      alt=${image.description}
+    />
+  </a></li>`).join("");
+
+gallery.innerHTML = markup;
+
+new Simplelightbox('.gallery a', {captionsData: "alt",  captionDelay: 250,});
